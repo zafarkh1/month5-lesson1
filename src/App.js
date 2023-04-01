8d9eb7641b1d1b8507829bb4ea486caa0436bc7e
zafarkh <hatamovzafar7070@gmail.com>
2023-04-01 12:38:59 +0500

    deleted master

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import Navbar from './components/navbar/Navbar'
-import Showcase from "./components/showcase/Showcase";
-import Popular from "./components/popular/Popular";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-      <Navbar />
-      <Showcase />
-      <Popular />
-    </div>
+    <div className="App"></div>
   );
 }
 
