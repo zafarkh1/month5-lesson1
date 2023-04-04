4d51bf108917bb21c7dbaaa17732fc64c49ccd81
zafarkh <hatamovzafar7070@gmail.com>
2023-04-04 17:00:15 +0500

    Rest Country

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,12 +7,13 @@ import './home.scss'
 const Home = () => {
 	const [data, setData] = useState([])
 	const [error, setError] = useState(null)
+	const [searchTitile, setSearchTitle] = useState("")
 
 	const fetchedData = async () => {
 		try {
 			const response = await axios.get(`https://restcountries.com/v3.1/all`)
 			setData(response.data)
-			console.log(response.data);
+			// console.log(response.data);
 		} catch (error) {
 			setError(error)
 		}
@@ -26,24 +27,44 @@ const Home = () => {
     return <div>Error: {error.message}</div>
     }
 
+		const filterData = data
+      .filter((value) => {
+        if (searchTitile === "") {
+          return value;
+        } else if (
+          value.name.common &&
+          value.name.common.toLowerCase().includes(searchTitile.toLowerCase())
+        ) {
+          return value;
+        }
+      })
+      .map((country) => (
+        <Country
+          flag={country.flags.png}
+          language={
+            country.languages &&
+            Object.keys(country.languages).map((key) => {
+              return country.languages[key];
+            })
+          }
+          country={country.name.common}
+          key={country.name.common}
+          id={country.name.common}
+          population={country.population}
+        />
+      ));
+
+
 	return (
-		<div className='wrapper'>
-			{
-				data.map((country) => (
-					<Country
-					flag = {country.flags.png}
-					language = {country.languages && Object.keys(country.languages).map((key) => {
-						return country.languages[key]
-					})}
-					country = {country.name.common}
-					key = {country.name.common}
-					id = {country.name.common}
-					population = {country.population}
-					/>
-				))
-			}
-		</div>
-	)
+    <div className='nnu'>
+      <input
+        className="home__text--input"
+        placeholder="Search"
+        onChange={(e) => setSearchTitle(e.target.value)}
+      />
+      <div className="wrapper">{filterData}</div>
+    </div>
+  );
 }
 
 export default Home
--- src/components/useParam/UseParam.jsx
+++ src/components/useParam/UseParam.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import { useParams, Link } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 const UseParam = () => {
 	const [data, setData] = useState([])
